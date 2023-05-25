755adb28e80406344d66fd8f9559bdecf2a2589f
Sarahsegla <sarahsegla@icloud.com>
2023-05-25 13:00:35 +0100

    whole modal hides and show onclick

--- script.js
+++ script.js
@@ -1,20 +1,6 @@
-// let back = document.querySelector(".back");
-// let modal = document.querySelector(".modal");
-
-
-
-// let button = document.querySelector(".logButton");
-// function ModalUp () {
-    
-//     back.style.display = 'block';
-//     modal.style.display = 'block';
-
-// };
-
-// ModalUp();
 
 $(document).ready(function() {
 $('.logButton').click(function() {
-    $('.back').toggle()
+    $('.wholeModal').toggle()
 }) 
 });
