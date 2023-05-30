0e52e9a27b644946b6fe46d11c2c04ebc8fc4dc7
Sarahsegla <sarahsegla@icloud.com>
2023-05-30 12:54:05 +0100

    change back to 100%

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-
+// make sure code run once the page is ready
 $(document).ready(function() {
+    // selecting the 'login' button by class for a click function
 $('.logButton').click(function() {
+    // selecting the whole modal by class to show once being clicked
     $('.wholeModal').toggle()
 }) 
 });
