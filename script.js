edf9b85c289056cda471192a01a6771eaf1181f8
Sarahsegla <sarahsegla@icloud.com>
2023-05-31 13:20:14 +0100

    change div to section. create divider div between button. works

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 // make sure code run once the page is ready
 $(document).ready(function() {
     // selecting the 'login' button by class for a click function
-$('.logButton').click(function() {
+$('.logButton').click(function(){
     // selecting the whole modal by class to show once being clicked
-    $('.wholeModal').toggle()
+    $('section').toggle()
 }) 
 });
+
+
+
+
